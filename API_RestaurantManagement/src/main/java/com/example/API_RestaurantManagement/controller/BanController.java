c019a21b32ff131606c81e4307ff33b568e95aa9
thuha <thuhango0204@gmail.com>
2022-09-15 15:20:40 +0700

    test

--- API_RestaurantManagement/src/main/java/com/example/API_RestaurantManagement/controller/BanController.java
+++ API_RestaurantManagement/src/main/java/com/example/API_RestaurantManagement/controller/BanController.java
@@ -2,6 +2,7 @@ package com.example.API_RestaurantManagement.controller;
 
 import com.example.API_RestaurantManagement.dto.BanDTO;
 import com.example.API_RestaurantManagement.service.BanService;
+import com.example.API_RestaurantManagement.service.CT_BanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,9 @@ import java.util.List;
 public class BanController {
     @Autowired
     private BanService banService;
+
+    @Autowired
+    private CT_BanService ct_banService;
 
     @GetMapping("")
     public List<BanDTO> layDSBan() {
